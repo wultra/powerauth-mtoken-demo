c8eda7fee0a483c52bd107d01540ddb58978f5af
Radek Holý <radek.holy@applifting.io>
2022-01-26 16:20:54 +0100

    Add EmailDto DTO

---
+++ src/main/java/com/wultra/demo/mtoken/data/dto/EmailDto.java
@@ -0,0 +1,39 @@
+/*
+ * Wultra Mobile Token Demo
+ * Copyright 2022 Wultra s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wultra.demo.mtoken.data.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class EmailDto {
+    @NotBlank
+    @Email
+    @Size(min = 5, max = 255)
+    @Schema(description = "The user's email address.", example = "john.doe@example.com")
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
